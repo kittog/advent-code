8ba63509ba82f4c89b146fc0164ee2962bcdce13
kittog <lena.gaubert@gmail.com>
2023-12-02 22:14:07 +0100

    part2!!!

---
+++ 02/game_power.py
@@ -0,0 +1,27 @@
+import re
+import numpy as np
+
+def game_power(game):
+    # returns power for one  game
+    split = game.split(":")
+    game_id, sets = int(re.findall(r'\d+', split[0])[0]), split[1]
+    cols = {"red" : [int(i) for i in re.findall(r'\d+(?= red)', sets)],
+    "blue" : [int(i) for i in re.findall(r'\d+(?= blue)', sets)],
+    "green" : [int(i) for i in re.findall(r'\d+(?= green)', sets)]}
+    power = 1
+    print(cols)
+    for col in cols.keys():
+        power *= np.max(cols[col])
+    return power
+
+def main():
+    with open("02/input.txt", "r") as f:
+        txt = f.read().strip()
+    games = txt.split("\n")
+    # colours = {"red":12, "green":13, "blue":14}
+    sum_power = 0
+    for game in games:
+        sum_power += game_power(game)
+    print(sum_power)
+
+main()

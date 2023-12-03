fd8613ceda67fd101c36141fc3f46d9de43d13ab
kittog <lena.gaubert@gmail.com>
2023-12-03 23:48:02 +0100

    not perfect but working!!

--- 03/day3.py
+++ 03/day3.py
@@ -2,24 +2,22 @@
 import re
 import numpy as np
 
-
-def init_last(engines, id):
+def init(engine0, engine1):
     s = 0
-    id_next = 1 if id==0 else -2
-    print(id_next)
-    l = engines[id]
-    l_next = engines[id_next]
-    print(l_next)
-    res = re.finditer('\d+', l)
+    res = re.finditer(r'\d+', engine0)
     for r in res:
         n = r.group()
-        print(n)
         a = r.start()-1 if r.start() != 0 else r.start()
-        b = r.end()+1 if r.end() != len(l) else r.end()
-        if re.findall(r'[^\w\.]', l[a:b]):
+        b = r.end()+1 if r.end() != len(engine0) else r.end()
+        if re.findall(r'[^\w\.]', engine0[a:b]):
+            # part number
             s += int(n)
-        elif re.findall(r'[^\w\.]', l_next[a:b]):
+        elif re.findall(r'[^\w\.]', engine1[a:b]):
+            print(re.findall(r'[^\w\.]', engine1[a:b]))
             s += int(n)
+        else:
+            continue
+        print(a, b, n, s)
     return s
 
 def core(engines):
@@ -49,24 +47,17 @@ def core(engines):
 
 def main():
     with open("03/input.txt", "r") as f:
-        txt = f.read()
-    sample = '''467..114..
-            ...*......
-            ..35..633.
-            ......#...
-            617*......
-            .....+.58.
-            ..592.....
-            ......755.
-            ...$.*....
-            .664.598..'''
-    engines = sample.split("\n")
+        txt = f.read().strip()
+    
+    engines = txt.split("\n")
+    print(engines)
     # init
-    sum_init = init_last(engines, 0)
+    sum_init = init(engines[0], engines[1])
     # 1 to n-1
     sum_core = core(engines)
     # last
-    sum_last = init_last(engines, 1)
-    print(sum_init+sum_core+sum_last)
+    sum_last = init(engines[-1], engines[-2])
+    print(sum_init, sum_core, sum_last)
+    #print(sum_init)
 
 main()
